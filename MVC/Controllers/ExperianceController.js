1fe795a9ba613b480aaa45063ee1c335f7dd23dd
Yalagala Srinivas <srinivas.y@lionorbit.com>
2025-07-28 14:32:21 +0530

    first commit

--- MVC/Controllers/ExperianceController.js
+++ MVC/Controllers/ExperianceController.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import experienceSchema from '../Models/ExperianceModel.js';
-import { sendResponse,sendErrorResponse } from '../MiddleWares/Response.js';
+import { sendResponse, sendErrorResponse } from '../MiddleWares/Response.js';
 import { checkAuth } from '../MiddleWares/CheckAuth.js';
 import mongoose from 'mongoose';
 const experienceRouter = express.Router();
@@ -8,15 +8,20 @@ const experienceRouter = express.Router();
 
 experienceRouter.post('/add', checkAuth, async (req, res, next) => {
     try {
-        const userId = { "userId": req.userId };
         const bodyData = req.body;
-        const experienceData = { ...userId, ...bodyData };
-        const result = await experienceSchema.create(experienceData);
+        const experienceData = Object.entries(bodyData)
+            .filter(([key]) => !isNaN(key))
+            .map(([_, exp]) => ({
+                ...exp,
+                userId: req.userId
+            }));
+        console.log(experienceData);
+        const result = await experienceSchema.insertMany(experienceData);
         if (result) {
-            sendResponse(res, true, "Experiance added successfully", result);
+        sendResponse(res, true, "Experiance added successfully", result);
         }
         else {
-            sendResponse(res, false, "Failed to add experiance", result);
+        sendResponse(res, false, "Failed to add experiance", result);
         }
     } catch (error) {
         sendErrorResponse(res, false, error.message);
@@ -24,14 +29,14 @@ experienceRouter.post('/add', checkAuth, async (req, res, next) => {
 });
 
 
-experienceRouter.post('/getById',checkAuth,async (req,res,next)=>{
+experienceRouter.post('/getById', checkAuth, async (req, res, next) => {
     try {
-        const queryKey = {"userId": new mongoose.Types.ObjectId(req.userId) };
+        const queryKey = { "userId": new mongoose.Types.ObjectId(req.userId) };
         const result = await experienceSchema.find(queryKey);
-        if(result){
+        if (result) {
             sendResponse(res, true, "Experiance found successfully", result);
         }
-        else{
+        else {
             sendResponse(res, false, "Failed to found experiance", result);
         }
     } catch (error) {
@@ -39,7 +44,7 @@ experienceRouter.post('/getById',checkAuth,async (req,res,next)=>{
     }
 });
 
-experienceRouter.get('/getAll',async (req,res,next)=>{
+experienceRouter.get('/getAll', async (req, res, next) => {
     const result = await experienceSchema.find();
     console.log(result);
 
