4fa6a84cf6beda274da98d058e1723868322364e
Yalagala Srinivas <srinivas.y@lionorbit.com>
2025-07-29 10:48:09 +0530

    Role Id Removal

--- MVC/Models/HealthCareWorkerModel.js
+++ MVC/Models/HealthCareWorkerModel.js
@@ -8,11 +8,11 @@ const healthCareWorkerSchema = new mongoose.Schema(
         email: { type: String, required: true, unique: true, lowercase: true, set: encrypt, get: decrypt },
         mobileNumber: { type: String, required: true, unique: true, trim: true, set: encrypt, get: decrypt },
         gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
-        dob: { type: Date },
         password: { type: String, set: encrypt },
+        dob: { type: Date },
         imageUrl: { type: String },
         verificationStatus: { type: String, required: true, enum: ["Pending", "Verified"], default: "Pending" },
-        role: { type: String, required: true },
+        role: { type: String },
         accountStatus: { type: String, required: true, enum: ["Active", "Inactive", "Suspended"], default: "Active" }
     },
     {
