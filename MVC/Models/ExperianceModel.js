b886227d8ff728dc39d1447e3af7fb2af3c86dcd
Yalagala Srinivas <srinivas.y@lionorbit.com>
2025-07-31 23:29:40 +0530

    Experiance Controller Moved

--- MVC/Models/ExperianceModel.js
+++ MVC/Models/ExperianceModel.js
@@ -4,7 +4,7 @@ const experienceSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, required: true },
     company: { type: String, required: true },
     jobTitle: { type: String, required: true },
-    startDate: { type: String, required: true },
+    startDate: { type: String },
     endDate: { type: String },
     isCurrentJob: { type: String, enum: ["Yes", "No"]},
     skills: { type: String }
