1bbc55ffb7240d2e43e15fd7a5627e30ca7ae894
Yalagala Srinivas <srinivas.y@lionorbit.com>
2025-07-31 15:13:44 +0530

    Experiance and Bank Data Controller

--- MVC/Models/ExperianceModel.js
+++ MVC/Models/ExperianceModel.js
@@ -4,8 +4,8 @@ const experienceSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, required: true },
     company: { type: String, required: true },
     jobTitle: { type: String, required: true },
-    startDate: { type: Date, required: true },
-    endDate: { type: Date }, // Optional, if currently working
+    startDate: { type: String, required: true },
+    endDate: { type: String }, // Optional, if currently working
     isCurrentJob: { type: Boolean, default: false },
     description: { type: String },
     skills: [{ type: String }] // List of skills gained in this experience
