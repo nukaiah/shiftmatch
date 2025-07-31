7865759b5df87e8d6d92a2b1da68ffa0b341b08d
Yalagala Srinivas <srinivas.y@lionorbit.com>
2025-07-31 17:57:13 +0530

    Qualification Details Updated

--- MVC/Controllers/QualificationController.js
+++ MVC/Controllers/QualificationController.js
@@ -7,36 +7,49 @@ import { checkAuth } from '../MiddleWares/CheckAuth.js';
 const qualificationRouter = express.Router();
 
 
-qualificationRouter.post('/add',checkAuth,async(req,res,next)=>{
+qualificationRouter.post('/add', checkAuth, async (req, res, next) => {
     try {
-        const userId = {"userId":req.userId};
+        const userId = { "userId": req.userId };
         const bodyData = req.body;
-        const qualificationData = { ...userId, ...bodyData};
-        const result = await qualificationSchema.create(qualificationData);
-        if(result){
-            sendResponse(res,true,"Qualification added successfully",result);
+        const qualificationData = { ...userId, ...bodyData };
+        const recordId = req.body._id;
+        if (recordId === null || recordId === "") {
+            const result = await qualificationSchema.create(qualificationData);
+            if (result) {
+                sendResponse(res, true, "Qualification added successfully", result);
+            }
+            else {
+                sendResponse(res, false, "Failed to add qualification", result);
+            }
         }
         else{
-            sendResponse(res,false,"Failed to add qualification",result);
+            const result = await qualificationSchema.updateOne({"_id":recordId},{$set:qualificationData});
+            if (result) {
+                sendResponse(res, true, "Qualification updated successfully", result);
+            }
+            else {
+                sendResponse(res, false, "Failed to update qualification", result);
+            }
         }
+
     } catch (error) {
-        sendErrorResponse(res,false,error.message);
+        sendErrorResponse(res, false, error.message);
     }
 });
 
 
-qualificationRouter.post('/getById',checkAuth,async (req,res,next)=>{
+qualificationRouter.post('/getById', checkAuth, async (req, res, next) => {
     try {
         const queryKey = { "userId": new mongoose.Types.ObjectId(req.userId) };
         const result = await qualificationSchema.find(queryKey);
-        if(result){
-            sendResponse(res,true,"Qualification Found",result);
+        if (result) {
+            sendResponse(res, true, "Qualification Found", result);
         }
-        else{
-            sendResponse(res,false,"Qualification Found",result);
+        else {
+            sendResponse(res, false, "Qualification Found", result);
         }
     } catch (error) {
-        sendErrorResponse(res,false,error.message);
+        sendErrorResponse(res, false, error.message);
     }
 });
 
