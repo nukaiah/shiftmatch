65062deec0d6b3dce4c23a20321f53b37b5b0580
Yalagala Srinivas <srinivas.y@lionorbit.com>
2025-07-31 16:10:47 +0530

    Qualification Details Updated

--- MVC/Models/QualifaicationModel.js
+++ MVC/Models/QualifaicationModel.js
@@ -2,13 +2,12 @@ import mongoose from "mongoose";
 
 const qualificationSchema = new mongoose.Schema({
     userId:{type:mongoose.Schema.Types.ObjectId,required: true},
-    degree: { type: String, required: true }, // e.g., Bachelor of Science
-    fieldOfStudy: { type: String, required: true }, // e.g., Computer Science
+    education:{type:String,required:true}, // e.g., Class X
     institution: { type: String, required: true }, // e.g., Harvard University
+    course: { type: String, required: true }, // e.g., Bachelor of Science
+    specialization: { type: String, required: true }, // e.g., Computer Science
     startYear: { type: Number, required: true }, // e.g., 2015
     endYear: { type: Number }, // e.g., 2019 (nullable in case the degree is ongoing)
-    grade: { type: String }, // e.g., GPA 3.8
-    description: { type: String } ,// Optional: Additional details
 },{ timestamps: true });
 
 export default mongoose.model("Qualifications", qualificationSchema);
